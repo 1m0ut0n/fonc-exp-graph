7e1db9551890a3bdb84156fff670ccc034923942
AMELEF <126089253+AMELEF@users.noreply.github.com>
2023-03-20 15:55:24 +0100

    Update du fichier erreurs.py
    Ajout des erreurs trouvées au niveau de l'évaluateur ainsi que leur prompt.

--- erreurs.py
+++ erreurs.py
@@ -50,7 +50,10 @@ class ErreurSyntax(Enum) :
 # Erreurs liées à l'évaluateur (syntaxe : ErreurEval.NOM_ERREUR)
 class ErreurEval(Enum):
     PAS_D_ERREUR = 400
-    #... (à ajouter au fur et à mesure)
+    ITERATION_NULLE_OU_NEGATIVE = 401 # Nombre d'itérations inférieur ou égal à 0
+    XMIN_SUPERIEUR_A_XMAX = 402 # xmin > xmax, erreur d'intervalle
+    XMIN_DIFFERENT_XMAX = 403 # Si nombre d'itérations = 1 et xmin n'est pas égal à xmax
+    FONCTION_VIDE = 404 # Erreur de syntaxe fonction vide : sqrt( ), sin ( ), 3+5+, 4^, etc. 
 
 
 
@@ -65,7 +68,7 @@ promptErreur = {
 promptErreurLex = {
     ErreurLex.PAS_D_ERREUR : "Il n'y a aucune erreur.",
     ErreurLex.FONCTION_MAL_ECRITE : "La fonction n'est pas reconnue.",
-    ErreurLex.RIEN_EST_TAPE_PAR_UTILISATEUR : "Veuillez inserer une fonction.",
+    ErreurLex.RIEN_EST_TAPE_PAR_UTILISATEUR : "Veuillez insérer une fonction.",
     ErreurLex.CARACTERE_NON_RECONNU : "La fonction inclue un caractère non reconnu."
 }
 
@@ -75,13 +78,17 @@ promptErreurSyntax = {
     ErreurSyntax.RECHERCHE_FIN_PARENTHESE_PAS_DE_PARENTHESE_OUVRANTE : "Une parenthèse ouvrante est manquante dans la fonction.",
     ErreurSyntax.RECHERCHE_FIN_PARENTHESE_PAS_DE_PARENTHESE_FERMANTE_CORRESPONDANTE : "Une parenthèse fermante est manquante dans la fonction.",
     ErreurSyntax.LISTE_VIDE : "Veuillez inserer une fonction.",
-    ErreurSyntax.ERREUR_INCONNUE : "Une erreur inconnue est survenue, veuillez nous en excusez.",
+    ErreurSyntax.ERREUR_INCONNUE : "Une erreur inconnue est survenue, veuillez nous en excuser.",
     ErreurSyntax.OPERATEUR_SANS_VALEUR_A_COTE : "Un operateur ne peut être isolé, il faut une valeur de part et d'autre de ce dernier.",
     ErreurSyntax.FONCTION_SANS_PARETHESE : "Une fonction ne peut être employée sans parenthèse pour indiquer son paramètre.",
-    ErreurSyntax.OBJETS_INCOMPATIBLE : "De objets mathèmatiques situés côte à côte sont imcompatibles entre eux."
+    ErreurSyntax.OBJETS_INCOMPATIBLE : "Des objets mathématiques situés côte à côte sont incompatibles entre eux."
 }
 
 # Prompts pour les erreurs liées à l'évaluateur (syntaxe : promptErreurEval[ErreurEval.NOM_ERREUR])
 promptErreurEval = {
-    ErreurEval.PAS_D_ERREUR : "Il n'y a aucune erreur."
+    ErreurEval.PAS_D_ERREUR : "Il n'y a aucune erreur.",
+    ErreurEval.ITERATION_NULLE_OU_NEGATIVE : "Le nombre d'itérations de calcul d'image ne peut être inférieur ou égal à 0.",
+    ErreurEval.XMIN_SUPERIEUR_A_XMAX : "La plus petite valeur de x ne peut pas être supérieur à la plus grande.",
+    ErreurEval.XMIN_DIFFERENT_XMAX : "Pour le calcul d'une seule image, l'intervalle doit être restreint à une seule valeur, xmin doit être égal à xmax.",
+    ErreurEval.FONCTION_VIDE : "Des objets mathématiques sont manquants (fonction vide)."
 }
