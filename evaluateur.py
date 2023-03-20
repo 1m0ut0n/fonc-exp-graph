19efcea72ff5a9ed2dc0adc188d76ef613d3f7bd
AMELEF <126089253+AMELEF@users.noreply.github.com>
2023-03-20 15:59:21 +0100

    Update du fichier evaluateur.py
    - Correction en fonction des modifications apportées au listage des erreurs
    - Ajout du repérage de l'erreur liée à une fonction vide ( sin( ), sqrt( ), ^5, etc.)
    - La fonction contient 3 arbres de test : arbretest1, arbretest2 et arbretest3

--- evaluateur.py
+++ evaluateur.py
@@ -1,10 +1,6 @@
 import math
 import common as c
-import syntax as syn
-
-nomb = eval(input("Entrez votre nombre d'itérations: \n"))
-xmin = eval(input("Entrez votre xmin: \n"))
-xmax = eval(input("Entrez votre xmax: \n"))
+import erreurs as er
 
 #Arbre de test 1
 jet1 = c.Jeton(c.Lexeme.OPERATEUR, c.Operateur.ADDITION)
@@ -16,15 +12,19 @@ jet6 = c.Jeton(c.Lexeme.REEL, 4)
 jet7 = c.Jeton(c.Lexeme.REEL, 5)
 jet8 = c.Jeton(c.Lexeme.VARIABLE)
 
-arbre1 = c.ArbreJeton(jet1)
-arbre1.insert_gauche(jet2)
-arbre1.insert_droit(jet3)
-arbre1.fils_gauche.insert_gauche(jet4)
-arbre1.fils_gauche.insert_droit(jet5)
-arbre1.fils_gauche.fils_gauche.insert_gauche(jet8)
-arbre1.fils_droit.insert_droit(jet7)
-arbre1.fils_droit.insert_gauche(jet6)
+arbretest1 = c.ArbreJeton(jet1)
+arbretest1.insert_gauche(jet2)
+arbretest1.insert_droit(jet3)
+arbretest1.fils_gauche.insert_gauche(jet4)
+arbretest1.fils_gauche.insert_droit(jet5)
+arbretest1.fils_gauche.fils_gauche.insert_gauche(jet8)
+arbretest1.fils_droit.insert_droit(jet7)
+arbretest1.fils_droit.insert_gauche(jet6)
 #Fin de l'arbre de test 1
+# f(x) = sqrt(x)/3 + 4^5
+# Résultat attendu pour evaluateur(arbretest2,10,-5,4):
+# [[-5.0, -4.0, -3.0, -2.0, -1.0, 0.0, 1.0, 2.0, 3.0, 4.0], 
+# [None, None, None, None, None, 1024.0, 1024.3333333333333, 1024.471404520791, 1024.5773502691895, 1024.6666666666667]]
 
 #Arbre de test 2
 jet1 = c.Jeton(c.Lexeme.OPERATEUR, c.Operateur.ADDITION)
@@ -36,16 +36,41 @@ jet6 = c.Jeton(c.Lexeme.REEL, -1)
 jet7 = c.Jeton(c.Lexeme.VARIABLE)
 jet8 = c.Jeton(c.Lexeme.VARIABLE)
 
-arbre2 = c.ArbreJeton(jet1)
-arbre2.insert_gauche(jet2)
-arbre2.insert_droit(jet3)
-arbre2.fils_gauche.insert_gauche(jet4)
-arbre2.fils_gauche.insert_droit(jet5)
-arbre2.fils_gauche.fils_gauche.insert_gauche(jet8)
-arbre2.fils_droit.insert_droit(jet7)
-arbre2.fils_droit.insert_gauche(jet6)
+arbretest2 = c.ArbreJeton(jet1)
+arbretest2.insert_gauche(jet2)
+arbretest2.insert_droit(jet3)
+arbretest2.fils_gauche.insert_gauche(jet4)
+arbretest2.fils_gauche.insert_droit(jet5)
+arbretest2.fils_gauche.fils_gauche.insert_gauche(jet8)
+arbretest2.fils_droit.insert_droit(jet7)
+arbretest2.fils_droit.insert_gauche(jet6)
 #Fin de l'arbre de test 2
+# f(x) = sqrt(x)/3 + (-1)^x
+# Résultat attendu pour evaluateur(arbretest2,10,-5,4): 
+#[[-5.0, -4.0, -3.0, -2.0, -1.0, 0.0, 1.0, 2.0, 3.0, 4.0],
+#[None, None, None, None, None, 1.0, -0.6666666666666667, 1.4714045207910318, -0.42264973081037427, 1.6666666666666665]]
 
+#Arbre de test 3
+jet1 = c.Jeton(c.Lexeme.OPERATEUR, c.Operateur.ADDITION)
+jet2 = c.Jeton(c.Lexeme.OPERATEUR, c.Operateur.DIVISION)
+jet3 = c.Jeton(c.Lexeme.OPERATEUR, c.Operateur.PUISSANCE)
+jet4 = c.Jeton(c.Lexeme.FONCTION, c.Fonction.SQRT)
+jet5 = c.Jeton(c.Lexeme.REEL, 3)
+jet6 = c.Jeton(c.Lexeme.REEL, -1)
+jet7 = c.Jeton(c.Lexeme.VARIABLE)
+
+
+arbretest3 = c.ArbreJeton(jet1)
+arbretest3.insert_gauche(jet2)
+arbretest3.insert_droit(jet3)
+arbretest3.fils_gauche.insert_gauche(jet4)
+arbretest3.fils_gauche.insert_droit(jet5)
+arbretest3.fils_droit.insert_droit(jet7)
+arbretest3.fils_droit.insert_gauche(jet6)
+#Fin de l'arbre de test 3
+# f(x) = sqrt()/3 + (-1)^x
+#Contient une erreur de syntaxe : sqrt(∅)
+#Résultat attendu : FONCTION_VIDE (erreur de syntaxe telle que sin(∅), sqrt(∅), ∅^k, k+∅, k*∅...)
 
 
 def postorderTraversal(root, x):
@@ -56,94 +81,167 @@ def postorderTraversal(root, x):
             return root.jeton.valeur
         elif root.jeton.lexeme == c.Lexeme.OPERATEUR:
             if root.jeton.valeur == c.Operateur.ADDITION:
+                if root.fils_gauche is None :
+                  return 'fonctionvide'
+                if root.fils_droit is None :
+                  return 'fonctionvide'
                 fg = postorderTraversal(root.fils_gauche,x)
                 fd = postorderTraversal(root.fils_droit,x)
+                #test d'une erreur du domaine de définition mathématique
                 if fg == 'erreurdef':
                   return 'erreurdef'
                 if fd == 'erreurdef':
                   return 'erreurdef'
+                #test d'une erreur de syntaxe (fonction vide)
+                if fg == 'fonctionvide':
+                  return 'fonctionvide'
+                if fd == 'fonctionvide':
+                  return 'fonctionvide'
                 return fg + fd
             elif root.jeton.valeur == c.Operateur.MULTIPLICATION:
+                if root.fils_gauche is None :
+                  return 'fonctionvide'
+                if root.fils_droit is None :
+                  return 'fonctionvide'
                 fg = postorderTraversal(root.fils_gauche,x)
                 fd = postorderTraversal(root.fils_droit,x)
+                #test d'une erreur du domaine de définition mathématique
                 if fg == 'erreurdef':
                   return 'erreurdef'
                 if fd == 'erreurdef':
                   return 'erreurdef'
+                #test d'une erreur de syntaxe (fonction vide)
+                if fg == 'fonctionvide':
+                  return 'fonctionvide'
+                if fd == 'fonctionvide':
+                  return 'fonctionvide'
                 return fg * fd
             elif root.jeton.valeur == c.Operateur.SOUSTRACTION:
+                if root.fils_gauche is None :
+                  return 'fonctionvide'
+                if root.fils_droit is None :
+                  return 'fonctionvide'
                 fg = postorderTraversal(root.fils_gauche,x)
                 fd = postorderTraversal(root.fils_droit,x)
+                #test d'une erreur du domaine de définition mathématique
                 if fg == 'erreurdef':
                   return 'erreurdef'
                 if fd == 'erreurdef':
                   return 'erreurdef'
+                #test d'une erreur de syntaxe (fonction vide)
+                if fg == 'fonctionvide':
+                  return 'fonctionvide'
+                if fd == 'fonctionvide':
+                  return 'fonctionvide'
                 return fg - fd
             elif root.jeton.valeur == c.Operateur.DIVISION:
+                if root.fils_gauche is None :
+                  return 'fonctionvide'
+                if root.fils_droit is None :
+                  return 'fonctionvide'
                 fg = postorderTraversal(root.fils_gauche,x)
                 fd = postorderTraversal(root.fils_droit,x)
+                #test d'une erreur du domaine de définition mathématique
                 if fg == 'erreurdef':
                   return 'erreurdef'
                 if fd == 'erreurdef':
                   return 'erreurdef'
+                #test d'une erreur de syntaxe (fonction vide)
+                if fg == 'fonctionvide':
+                  return 'fonctionvide'
+                if fd == 'fonctionvide':
+                  return 'fonctionvide'
                 if root.fils_droit.jeton.lexeme == c.Lexeme.VARIABLE and x == 0 :
                   x = 10e-300
                 if root.fils_droit.jeton.valeur == 0:
                   root.fils_droit.jeton.valeur = 10e-300
                 return fg/fd
             elif root.jeton.valeur == c.Operateur.PUISSANCE:
+                if root.fils_gauche is None :
+                  return 'fonctionvide'
+                if root.fils_droit is None :
+                  return 'fonctionvide'
                 fg = postorderTraversal(root.fils_gauche,x)
                 fd = postorderTraversal(root.fils_droit,x)
+                #test d'une erreur du domaine de définition mathématique
                 if fg == 'erreurdef':
                   return 'erreurdef'
                 if fd == 'erreurdef':
                   return 'erreurdef'
+                #test d'une erreur de syntaxe (fonction vide)
+                if fg == 'fonctionvide':
+                  return 'fonctionvide'
+                if fd == 'fonctionvide':
+                  return 'fonctionvide'
                 return pow(fg, fd)
         elif root.jeton.lexeme == c.Lexeme.FONCTION:
             if root.jeton.valeur == c.Fonction.ABS:
+                if root.fils_gauche is None :
+                  return 'fonctionvide'
                 fg = postorderTraversal(root.fils_gauche,x)
+                #test d'une erreur du domaine de définition mathématique
                 if fg == 'erreurdef':
                   return 'erreurdef'
+                #test d'une erreur de syntaxe (fonction vide)
+                if fg == 'fonctionvide':
+                  return 'fonctionvide'
                 return abs(fg)
             elif root.jeton.valeur == c.Fonction.SIN:
+                if root.fils_gauche is None :
+                  return 'fonctionvide'
                 fg = postorderTraversal(root.fils_gauche,x)
+                #test d'une erreur du domaine de définition mathématique
                 if fg == 'erreurdef':
                   return 'erreurdef'
+                #test d'une erreur de syntaxe (fonction vide)
+                if fg == 'fonctionvide':
+                  return 'fonctionvide'
                 return math.sin(fg)
             elif root.jeton.valeur == c.Fonction.SQRT:
+                if root.fils_gauche is None :
+                  return 'fonctionvide'
                 fg = postorderTraversal(root.fils_gauche,x)
+                #test d'une erreur du domaine de définition mathématique
                 if fg < 0:
                   fg = 'erreurdef'
                 if fg == 'erreurdef':
                   return 'erreurdef'
+                #test d'une erreur de syntaxe (fonction vide)
+                if fg == 'fonctionvide':
+                  return 'fonctionvide'
                 return math.sqrt(fg)
             elif root.jeton.valeur == c.Fonction.COS:
                 fg = postorderTraversal(root.fils_gauche,x)
+                #test d'une erreur du domaine de définition mathématique
                 if fg == 'erreurdef':
                   return 'erreurdef'
+                #test d'une erreur de syntaxe (fonction vide)
+                if fg == 'fonctionvide':
+                  return 'fonctionvide'
                 return math.cos(fg)
             elif root.jeton.valeur == c.Fonction.EXP:
                 fg = postorderTraversal(root.fils_gauche,x)
+                #test d'une erreur du domaine de définition mathématique
                 if fg == 'erreurdef':
                   return 'erreurdef'
+                #test d'une erreur de syntaxe (fonction vide)
+                if fg == 'fonctionvide':
+                  return 'fonctionvide'
                 return math.exp(fg)
         elif root.jeton.lexeme == c.Lexeme.VARIABLE:
             return x
 
 
-#func = postorderTraversal(arbre1, x)
-#print(func)
-
 
 
 def evaluateur(arbre,nomb, xmin, xmax):
     if nomb <= 0 :
-      return c.Erreur.ERREUR_301
+      return er.ErreurEval.ITERATION_NULLE_OU_NEGATIVE
     res = [[0] * nomb, [0] * nomb]
     if xmin > xmax : # Vérification de l'intervalle
-      return c.Erreur.ERREUR_302
+      return er.ErreurEval.XMIN_SUPERIEUR_A_XMAX
     if nomb == 1 and xmin != xmax: # S'il n'y a qu'une seule itération, on vérifie que xmin=xmax
-      return c.Erreur.ERREUR_303
+      return er.ErreurEval.XMIN_DIFFERENT_XMAX
     if nomb == 1: # S'il n'y a qu'une seule itération, on retourne le xmin et son image
       res[0][0] = xmin
       res[1][0] = postorderTraversal(arbre,xmin)
@@ -159,6 +257,8 @@ def evaluateur(arbre,nomb, xmin, xmax):
        res[0][i] = xmin + i*(xmax-xmin)/(nomb-1)
     for i in range(nomb):
         res[1][i] = postorderTraversal(arbre,res[0][i])
+        if res[1][i] == 'fonctionvide':
+          return er.ErreurEval.FONCTION_VIDE
         if res[1][i] == 'erreurdef':
             res[1][i] = None
         if isinstance(res[1][i], complex):
