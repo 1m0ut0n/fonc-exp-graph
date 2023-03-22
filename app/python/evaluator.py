fdd62183ea060cd0bafb59cdc117bc53d43dedb3
AMELEF <126089253+AMELEF@users.noreply.github.com>
2023-03-22 15:04:31 +0100

    Update evaluator.py
    - Ajout d'arbres de test (7 au total) servant à tester des cas pouvant poser problème
    - Ajout de tests sur les valeurs complexes qui généraient des bugs python (sqrt(complex), sin(complex) ...)

--- app/python/evaluator.py
+++ app/python/evaluator.py
@@ -2,137 +2,6 @@ import math
 import common as c
 import erreurs as er
 
-#------------------------------------ Arbres de tests ----------------------------------------------
-# Tous les "résultats attendus" sont faits avec l'appel : evaluateur(arbre, 10, -5, 4).
-#Arbre de test 1
-# On fabrique les jetons de chaque opération/fonction/valeur
-jet1 = c.Jeton(c.Lexeme.OPERATEUR, c.Operateur.ADDITION)
-jet2 = c.Jeton(c.Lexeme.OPERATEUR, c.Operateur.DIVISION)
-jet3 = c.Jeton(c.Lexeme.OPERATEUR, c.Operateur.PUISSANCE)
-jet4 = c.Jeton(c.Lexeme.FONCTION, c.Fonction.SQRT)
-jet5 = c.Jeton(c.Lexeme.REEL, 3)
-jet6 = c.Jeton(c.Lexeme.REEL, 4)
-jet7 = c.Jeton(c.Lexeme.REEL, 5)
-jet8 = c.Jeton(c.Lexeme.VARIABLE)
-
-# On remplit l'arbre avec les jetons
-arbretest1 = c.ArbreJeton(jet1)
-arbretest1.insert_gauche(jet2)
-arbretest1.insert_droit(jet3)
-arbretest1.fils_gauche.insert_gauche(jet4)
-arbretest1.fils_gauche.insert_droit(jet5)
-arbretest1.fils_gauche.fils_gauche.insert_gauche(jet8)
-arbretest1.fils_droit.insert_droit(jet7)
-arbretest1.fils_droit.insert_gauche(jet6)
-#Fin de l'arbre de test 1
-# f(x) = sqrt(x)/3 + 4^5
-# Résultat attendu pour evaluateur(arbretest1,10,-5,4): (<ErreurEval.PAS_D_ERREUR: 400>, [[-5.0, -4.0, -3.0, -2.0, -1.0, 0.0, 1.0, 2.0, 3.0, 4.0], [None, None, None, None, None, 1024.0, 1024.3333333333333, 1024.471404520791, 1024.5773502691895, 1024.6666666666667]])
-
-#Arbre de test 2
-jet1 = c.Jeton(c.Lexeme.OPERATEUR, c.Operateur.ADDITION)
-jet2 = c.Jeton(c.Lexeme.OPERATEUR, c.Operateur.DIVISION)
-jet3 = c.Jeton(c.Lexeme.OPERATEUR, c.Operateur.PUISSANCE)
-jet4 = c.Jeton(c.Lexeme.FONCTION, c.Fonction.SQRT)
-jet5 = c.Jeton(c.Lexeme.REEL, 3)
-jet6 = c.Jeton(c.Lexeme.REEL, -1)
-jet7 = c.Jeton(c.Lexeme.VARIABLE)
-jet8 = c.Jeton(c.Lexeme.VARIABLE)
-
-arbretest2 = c.ArbreJeton(jet1)
-arbretest2.insert_gauche(jet2)
-arbretest2.insert_droit(jet3)
-arbretest2.fils_gauche.insert_gauche(jet4)
-arbretest2.fils_gauche.insert_droit(jet5)
-arbretest2.fils_gauche.fils_gauche.insert_gauche(jet8)
-arbretest2.fils_droit.insert_droit(jet7)
-arbretest2.fils_droit.insert_gauche(jet6)
-#Fin de l'arbre de test 2
-# f(x) = sqrt(x)/3 + (-1)^x
-# Résultat attendu pour evaluateur(arbretest2,10,-5,4): (<ErreurEval.PAS_D_ERREUR: 400>, [[-5.0, -4.0, -3.0, -2.0, -1.0, 0.0, 1.0, 2.0, 3.0, 4.0], [None, None, None, None, None, 1.0, -0.6666666666666667, 1.4714045207910318, -0.42264973081037427, 1.6666666666666665]])
-
-#Arbre de test 3
-jet1 = c.Jeton(c.Lexeme.OPERATEUR, c.Operateur.ADDITION)
-jet2 = c.Jeton(c.Lexeme.OPERATEUR, c.Operateur.DIVISION)
-jet3 = c.Jeton(c.Lexeme.OPERATEUR, c.Operateur.PUISSANCE)
-jet4 = c.Jeton(c.Lexeme.FONCTION, c.Fonction.SQRT)
-jet5 = c.Jeton(c.Lexeme.REEL, 3)
-jet6 = c.Jeton(c.Lexeme.REEL, -1)
-jet7 = c.Jeton(c.Lexeme.VARIABLE)
-
-
-arbretest3 = c.ArbreJeton(jet1)
-arbretest3.insert_gauche(jet2)
-arbretest3.insert_droit(jet3)
-arbretest3.fils_gauche.insert_gauche(jet4)
-arbretest3.fils_gauche.insert_droit(jet5)
-arbretest3.fils_droit.insert_droit(jet7)
-arbretest3.fils_droit.insert_gauche(jet6)
-#Fin de l'arbre de test 3
-# f(x) = sqrt()/3 + (-1)^x
-#Contient une erreur de syntaxe : sqrt(∅)
-#Résultat attendu : (<ErreurEval.FONCTION_VIDE: 403>, [[-5.0, -4.0, -3.0, -2.0, -1.0, 0.0, 1.0, 2.0, 3.0, 4.0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]])
-
-#Arbre de test 4
-jet1 = c.Jeton(c.Lexeme.OPERATEUR, c.Operateur.DIVISION)
-jet2 = c.Jeton(c.Lexeme.FONCTION, c.Fonction.SIN)
-jet3 = c.Jeton(c.Lexeme.FONCTION, c.Fonction.EXP)
-jet4 = c.Jeton(c.Lexeme.FONCTION, c.Fonction.COS)
-jet5 = c.Jeton(c.Lexeme.OPERATEUR, c.Operateur.SOUSTRACTION)
-jet6 = c.Jeton(c.Lexeme.OPERATEUR, c.Operateur.PUISSANCE)
-jet7 = c.Jeton(c.Lexeme.OPERATEUR, c.Operateur.MULTIPLICATION)
-jet8 = c.Jeton(c.Lexeme.FONCTION, c.Fonction.LOG)
-jet9 = c.Jeton(c.Lexeme.FONCTION, c.Fonction.SQRT)
-jet10 = c.Jeton(c.Lexeme.REEL, 3)
-jet11 = c.Jeton(c.Lexeme.REEL, 2)
-jet12 = c.Jeton(c.Lexeme.VARIABLE)
-jet13 = c.Jeton(c.Lexeme.FONCTION, c.Fonction.ABS)
-jet14 = c.Jeton(c.Lexeme.VARIABLE)
-jet15 = c.Jeton(c.Lexeme.OPERATEUR, c.Operateur.ADDITION)
-jet16 = c.Jeton(c.Lexeme.VARIABLE)
-jet17 = c.Jeton(c.Lexeme.REEL, 3)
-
-arbretest4 = c.ArbreJeton(jet1)
-arbretest4.insert_gauche(jet2)
-arbretest4.insert_droit(jet3)
-arbretest4.fils_gauche.insert_gauche(jet4)
-arbretest4.fils_droit.insert_gauche(jet5)
-arbretest4.fils_gauche.fils_gauche.insert_gauche(jet6)
-arbretest4.fils_droit.fils_gauche.insert_gauche(jet7)
-arbretest4.fils_droit.fils_gauche.insert_droit(jet8)
-arbretest4.fils_gauche.fils_gauche.fils_gauche.insert_gauche(jet9)
-arbretest4.fils_gauche.fils_gauche.fils_gauche.insert_droit(jet10)
-arbretest4.fils_droit.fils_gauche.fils_gauche.insert_gauche(jet11)
-arbretest4.fils_droit.fils_gauche.fils_gauche.insert_droit(jet12)
-arbretest4.fils_droit.fils_gauche.fils_droit.insert_gauche(jet13)
-arbretest4.fils_gauche.fils_gauche.fils_gauche.fils_gauche.insert_gauche(jet14)
-arbretest4.fils_droit.fils_gauche.fils_droit.fils_gauche.insert_gauche(jet15)
-arbretest4.fils_droit.fils_gauche.fils_droit.fils_gauche.fils_gauche.insert_gauche(jet16)
-arbretest4.fils_droit.fils_gauche.fils_droit.fils_gauche.fils_gauche.insert_droit(jet17)
-
-#Fin de l'arbre de test 4
-# f(x) = sin(cos(sqrt(x)^3)) / exp(2*x-ln(|x+3|))
-#Résultat attendu : (<ErreurEval.PAS_D_ERREUR: 400>, [[-5.0, -4.0, -3.0, -2.0, -1.0, 0.0, 1.0, 2.0, 3.0, 4.0], [None, None, None, None, None, 2.5244129544236897, 0.27846331203142055, -0.07456366716803889, 0.006670690059206117, -0.0003404645052498309]])
-
-#Arbre de test 5
-jet1 = c.Jeton(c.Lexeme.OPERATEUR, c.Operateur.DIVISION)
-jet2 = c.Jeton(c.Lexeme.REEL, 5)
-jet3 = c.Jeton(c.Lexeme.REEL, 0)
-
-
-arbretest5 = c.ArbreJeton(jet1)
-arbretest5.insert_gauche(jet2)
-arbretest5.insert_droit(jet3)
-
-arbretest5.fils_droit.insert_gauche(jet6)
-#Fin de l'arbre de test 5
-# f(x) = 5/0 (division par 0)
-# Résultat attendu : (<ErreurEval.PAS_D_ERREUR: 400>, [[-5.0, -4.0, -3.0, -2.0, -1.0, 0.0, 1.0, 2.0, 3.0, 4.0], [None, None, None, None, None, None, None, None, None, None]])
-
-
-
-#---------------------------------------- Fin des arbres de tests -----------------------------------
-
-
 def postorderTraversal(root, x):
     if root is None :
         return ''
@@ -219,7 +88,7 @@ def postorderTraversal(root, x):
                   return 'fonctionvide'
                 if root.fils_droit.jeton.lexeme == c.Lexeme.VARIABLE and x == 0 :
                   return 'erreurdef'
-                if root.fils_droit.jeton.valeur == 0:
+                if fd == 0:
                   return 'erreurdef'
                 return fg/fd
             elif root.jeton.valeur == c.Operateur.PUISSANCE:
@@ -260,6 +129,8 @@ def postorderTraversal(root, x):
                 if root.fils_droit is None :
                   return 'fonctionvide'
                 fd = postorderTraversal(root.fils_droit,x)
+                if isinstance(fd, complex) : # On vérifie que le fils droit n'est pas complexe
+                  return 'erreurdef'
                 # Test qui sert à propager une erreur trouvée dans un des fils :
                 if fd == 'erreurdef':
                   return 'erreurdef'
@@ -271,6 +142,8 @@ def postorderTraversal(root, x):
                 if root.fils_droit is None :
                   return 'fonctionvide'
                 fd = postorderTraversal(root.fils_droit,x)
+                if isinstance(fd, complex) : # On vérifie que le fils droit n'est pas complexe
+                  return 'erreurdef'
                 # Test qui sert à propager une erreur trouvée dans un des fils :
                 if fd < 0:
                   fd = 'erreurdef'
@@ -284,6 +157,8 @@ def postorderTraversal(root, x):
                 if root.fils_droit is None :
                   return 'fonctionvide'
                 fd = postorderTraversal(root.fils_droit,x)
+                if isinstance(fd, complex) : # On vérifie que le fils droit n'est pas complexe
+                  return 'erreurdef'
                 # Test qui sert à propager une erreur trouvée dans un des fils :
                 if fd == 'erreurdef':
                   return 'erreurdef'
@@ -295,6 +170,8 @@ def postorderTraversal(root, x):
                 if root.fils_droit is None :
                   return 'fonctionvide'
                 fd = postorderTraversal(root.fils_droit,x)
+                if isinstance(fd, complex) : # On vérifie que le fils droit n'est pas complexe
+                  return 'erreurdef'
                 # Test qui sert à propager une erreur trouvée dans un des fils :
                 if fd == 'erreurdef':
                   return 'erreurdef'
@@ -306,7 +183,7 @@ def postorderTraversal(root, x):
                 if root.fils_droit is None :
                   return 'fonctionvide'
                 fd = postorderTraversal(root.fils_droit,x)
-                if isinstance(fd, complex) :
+                if isinstance(fd, complex) : # On vérifie que le fils droit n'est pas complexe
                   return 'erreurdef'
                 if fd <=0 :
                   fd = 'erreurdef'
@@ -342,3 +219,200 @@ def evaluateur(arbre,nomb, xmin, xmax):
         if isinstance(res[1][i], complex): # S'il a une image mais qu'elle est complexe, alors son image est un None.
             res[1][i] = None
     return er.ErreurEval.PAS_D_ERREUR, res # On retourne le code qui dit qu'il n'y a pas d'erreur et on retourne la liste contenant en première ligne les antécédants, et en seconde les images
+
+""""
+#------------------------------------ Arbres de tests ----------------------------------------------
+# Tous les "résultats attendus" sont faits avec l'appel : evaluateur(arbre, 10, -5, 4).
+#Arbre de test 1
+# On fabrique les jetons de chaque opération/fonction/valeur
+jet1 = c.Jeton(c.Lexeme.OPERATEUR, c.Operateur.ADDITION)
+jet2 = c.Jeton(c.Lexeme.OPERATEUR, c.Operateur.DIVISION)
+jet3 = c.Jeton(c.Lexeme.OPERATEUR, c.Operateur.PUISSANCE)
+jet4 = c.Jeton(c.Lexeme.FONCTION, c.Fonction.SQRT)
+jet5 = c.Jeton(c.Lexeme.REEL, 3)
+jet6 = c.Jeton(c.Lexeme.REEL, 4)
+jet7 = c.Jeton(c.Lexeme.REEL, 5)
+jet8 = c.Jeton(c.Lexeme.VARIABLE)
+
+# On remplit l'arbre avec les jetons
+arbretest1 = c.ArbreJeton(jet1)
+arbretest1.insert_gauche(jet2)
+arbretest1.insert_droit(jet3)
+arbretest1.fils_gauche.insert_gauche(jet4)
+arbretest1.fils_gauche.insert_droit(jet5)
+arbretest1.fils_gauche.fils_gauche.insert_droit(jet8)
+arbretest1.fils_droit.insert_droit(jet7)
+arbretest1.fils_droit.insert_gauche(jet6)
+#Fin de l'arbre de test 1
+# f(x) = sqrt(x)/3 + 4^5
+# Résultat attendu pour evaluateur(arbretest1,10,-5,4): (<ErreurEval.PAS_D_ERREUR: 400>, [[-5.0, -4.0, -3.0, -2.0, -1.0, 0.0, 1.0, 2.0, 3.0, 4.0], [None, None, None, None, None, 1024.0, 1024.3333333333333, 1024.471404520791, 1024.5773502691895, 1024.6666666666667]])
+
+#Arbre de test 2
+jet1 = c.Jeton(c.Lexeme.OPERATEUR, c.Operateur.ADDITION)
+jet2 = c.Jeton(c.Lexeme.OPERATEUR, c.Operateur.DIVISION)
+jet3 = c.Jeton(c.Lexeme.OPERATEUR, c.Operateur.PUISSANCE)
+jet4 = c.Jeton(c.Lexeme.FONCTION, c.Fonction.SQRT)
+jet5 = c.Jeton(c.Lexeme.REEL, 3)
+jet6 = c.Jeton(c.Lexeme.REEL, -1)
+jet7 = c.Jeton(c.Lexeme.VARIABLE)
+jet8 = c.Jeton(c.Lexeme.VARIABLE)
+
+arbretest2 = c.ArbreJeton(jet1)
+arbretest2.insert_gauche(jet2)
+arbretest2.insert_droit(jet3)
+arbretest2.fils_gauche.insert_gauche(jet4)
+arbretest2.fils_gauche.insert_droit(jet5)
+arbretest2.fils_gauche.fils_gauche.insert_droit(jet8)
+arbretest2.fils_droit.insert_droit(jet7)
+arbretest2.fils_droit.insert_gauche(jet6)
+#Fin de l'arbre de test 2
+# Sert à vérifier
+# f(x) = sqrt(x)/3 + (-1)^x
+# Résultat attendu pour evaluateur(arbretest2,10,-1,1): (<ErreurEval.PAS_D_ERREUR: 400>, [[-1.0, -0.7777777777777778, -0.5555555555555556, -0.33333333333333337, -0.11111111111111116, 0.11111111111111116, 0.33333333333333326, 0.5555555555555556, 0.7777777777777777, 1.0], [None, None, None, None, None, None, None, None, None, -0.6666666666666667]])
+
+#Arbre de test 3
+jet1 = c.Jeton(c.Lexeme.OPERATEUR, c.Operateur.ADDITION)
+jet2 = c.Jeton(c.Lexeme.OPERATEUR, c.Operateur.DIVISION)
+jet3 = c.Jeton(c.Lexeme.OPERATEUR, c.Operateur.PUISSANCE)
+jet4 = c.Jeton(c.Lexeme.FONCTION, c.Fonction.SQRT)
+jet5 = c.Jeton(c.Lexeme.REEL, 3)
+jet6 = c.Jeton(c.Lexeme.REEL, -1)
+jet7 = c.Jeton(c.Lexeme.VARIABLE)
+
+
+arbretest3 = c.ArbreJeton(jet1)
+arbretest3.insert_gauche(jet2)
+arbretest3.insert_droit(jet3)
+arbretest3.fils_gauche.insert_gauche(jet4)
+arbretest3.fils_gauche.insert_droit(jet5)
+arbretest3.fils_droit.insert_droit(jet7)
+arbretest3.fils_droit.insert_gauche(jet6)
+#Fin de l'arbre de test 3
+# f(x) = sqrt()/3 + (-1)^x
+#Contient une erreur de syntaxe : sqrt(∅)
+#Résultat attendu : (<ErreurEval.FONCTION_VIDE: 403>, [[-5.0, -4.0, -3.0, -2.0, -1.0, 0.0, 1.0, 2.0, 3.0, 4.0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]])
+
+#Arbre de test 4
+jet1 = c.Jeton(c.Lexeme.OPERATEUR, c.Operateur.DIVISION)
+jet2 = c.Jeton(c.Lexeme.FONCTION, c.Fonction.SIN)
+jet3 = c.Jeton(c.Lexeme.FONCTION, c.Fonction.EXP)
+jet4 = c.Jeton(c.Lexeme.FONCTION, c.Fonction.COS)
+jet5 = c.Jeton(c.Lexeme.OPERATEUR, c.Operateur.SOUSTRACTION)
+jet6 = c.Jeton(c.Lexeme.OPERATEUR, c.Operateur.PUISSANCE)
+jet7 = c.Jeton(c.Lexeme.OPERATEUR, c.Operateur.MULTIPLICATION)
+jet8 = c.Jeton(c.Lexeme.FONCTION, c.Fonction.LOG)
+jet9 = c.Jeton(c.Lexeme.FONCTION, c.Fonction.SQRT)
+jet10 = c.Jeton(c.Lexeme.REEL, 3)
+jet11 = c.Jeton(c.Lexeme.REEL, 2)
+jet12 = c.Jeton(c.Lexeme.VARIABLE)
+jet13 = c.Jeton(c.Lexeme.FONCTION, c.Fonction.ABS)
+jet14 = c.Jeton(c.Lexeme.VARIABLE)
+jet15 = c.Jeton(c.Lexeme.OPERATEUR, c.Operateur.ADDITION)
+jet16 = c.Jeton(c.Lexeme.VARIABLE)
+jet17 = c.Jeton(c.Lexeme.REEL, 3)
+
+arbretest4 = c.ArbreJeton(jet1)
+arbretest4.insert_gauche(jet2)
+arbretest4.insert_droit(jet3)
+arbretest4.fils_gauche.insert_droit(jet4)
+arbretest4.fils_droit.insert_droit(jet5)
+arbretest4.fils_gauche.fils_droit.insert_droit(jet6)
+arbretest4.fils_droit.fils_droit.insert_gauche(jet7)
+arbretest4.fils_droit.fils_droit.insert_droit(jet8)
+arbretest4.fils_gauche.fils_droit.fils_droit.insert_gauche(jet9)
+arbretest4.fils_gauche.fils_droit.fils_droit.insert_droit(jet10)
+arbretest4.fils_droit.fils_droit.fils_gauche.insert_gauche(jet11)
+arbretest4.fils_droit.fils_droit.fils_gauche.insert_droit(jet12)
+arbretest4.fils_droit.fils_droit.fils_droit.insert_droit(jet13)
+arbretest4.fils_gauche.fils_droit.fils_droit.fils_gauche.insert_droit(jet14)
+arbretest4.fils_droit.fils_droit.fils_droit.fils_droit.insert_droit(jet15)
+arbretest4.fils_droit.fils_droit.fils_droit.fils_droit.fils_droit.insert_gauche(jet16)
+arbretest4.fils_droit.fils_droit.fils_droit.fils_droit.fils_droit.insert_droit(jet17)
+
+#Fin de l'arbre de test 4
+# f(x) = sin(cos(sqrt(x)^3)) / exp(2*x-ln(|x+3|))
+#Résultat attendu : (<ErreurEval.PAS_D_ERREUR: 400>, [[-5.0, -4.0, -3.0, -2.0, -1.0, 0.0, 1.0, 2.0, 3.0, 4.0], [None, None, None, None, None, 2.5244129544236897, 0.27846331203142055, -0.07456366716803889, 0.006670690059206117, -0.0003404645052498309]])
+
+#Arbre de test 5
+jet1 = c.Jeton(c.Lexeme.OPERATEUR, c.Operateur.DIVISION)
+jet2 = c.Jeton(c.Lexeme.REEL, 5)
+jet3 = c.Jeton(c.Lexeme.REEL, 0)
+
+
+arbretest5 = c.ArbreJeton(jet1)
+arbretest5.insert_gauche(jet2)
+arbretest5.insert_droit(jet3)
+
+arbretest5.fils_droit.insert_gauche(jet6)
+#Fin de l'arbre de test 5
+# f(x) = 5/0 (division par 0)
+# Résultat attendu : (<ErreurEval.PAS_D_ERREUR: 400>, [[-5.0, -4.0, -3.0, -2.0, -1.0, 0.0, 1.0, 2.0, 3.0, 4.0], [None, None, None, None, None, None, None, None, None, None]])
+
+
+#Arbre de test 6
+jet1 = c.Jeton(c.Lexeme.OPERATEUR, c.Operateur.PUISSANCE)
+jet2 = c.Jeton(c.Lexeme.REEL, -1)
+jet3 = c.Jeton(c.Lexeme.OPERATEUR, c.Operateur.DIVISION)
+jet4 = c.Jeton(c.Lexeme.REEL, 1)
+jet5 = c.Jeton(c.Lexeme.VARIABLE)
+
+arbretest6 = c.ArbreJeton(jet1)
+arbretest6.insert_gauche(jet2)
+arbretest6.insert_droit(jet3)
+arbretest6.fils_droit.insert_gauche(jet4)
+arbretest6.fils_droit.insert_droit(jet5)
+
+#Fin de l'arbre de test 6
+# f(x) = (-1)^(1/x) (contient des images compelxes et une division par 0)
+# Résultat attendu : (<ErreurEval.PAS_D_ERREUR: 400>, [[-5.0, -4.0, -3.0, -2.0, -1.0, 0.0, 1.0, 2.0, 3.0, 4.0], [None, None, None, None, -1.0, None, -1.0, None, None, None]])
+
+#Arbre de test 7
+jet1 = c.Jeton(c.Lexeme.FONCTION, c.Fonction.ABS)
+jet2 = c.Jeton(c.Lexeme.OPERATEUR, c.Operateur.PUISSANCE)
+jet3 = c.Jeton(c.Lexeme.FONCTION, c.Fonction.SIN)
+jet4 = c.Jeton(c.Lexeme.OPERATEUR, c.Operateur.DIVISION)
+jet5 = c.Jeton(c.Lexeme.VARIABLE)
+jet6 = c.Jeton(c.Lexeme.REEL, 1)
+jet7 = c.Jeton(c.Lexeme.VARIABLE)
+
+arbretest7 = c.ArbreJeton(jet1)
+arbretest7.insert_droit(jet2)
+arbretest7.fils_droit.insert_gauche(jet3)
+arbretest7.fils_droit.insert_droit(jet4)
+arbretest7.fils_droit.fils_gauche.insert_droit(jet5)
+arbretest7.fils_droit.fils_droit.insert_gauche(jet6)
+arbretest7.fils_droit.fils_droit.insert_droit(jet7)
+
+#Fin de l'arbre de test 7
+# f(x) = |sin(x)^(1/x)| (contient des images complexes et une division par 0)
+# Résultat attendu : (<ErreurEval.PAS_D_ERREUR: 400>, [[-5.0, -4.0, -3.0, -2.0, -1.0, 0.0, 1.0, 2.0, 3.0, 4.0], [1.0084239171861806, 1.0721470652975371, 1.9207483515575248, 1.0486897397679718, 1.1883951057781212, None, 0.8414709848078965, 0.9535708819095106, 0.5206304090742055, 0.9327078647764472]])
+
+
+#---------------------------------------- Fin des arbres de tests -----------------------------------
+
+
+
+Affichage des arbres de test : 
+
+liste = evaluateur.evaluateur(evaluateur.arbretest1,10,-5,4)
+print(liste)
+print("\n")
+liste = evaluateur.evaluateur(evaluateur.arbretest2,10,-1,1)
+print(liste)
+print("\n")
+liste = evaluateur.evaluateur(evaluateur.arbretest3,10,-5,4)
+print(liste)
+print("\n")
+liste = evaluateur.evaluateur(evaluateur.arbretest4,10,-5,4)
+print(liste)
+print("\n")
+liste = evaluateur.evaluateur(evaluateur.arbretest5,10,-5,4)
+print(liste)
+print("\n")
+liste = evaluateur.evaluateur(evaluateur.arbretest6,10,-5,4)
+print(liste)
+print("\n")
+liste = evaluateur.evaluateur(evaluateur.arbretest7,10,-5,4)
+print(liste)
+print("\n")
+"""
